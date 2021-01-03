bed1245e63f46fac16d057b03a8dfa9af93fcc89
mert-alptekin <mert-alptekin@hotmail.com>
2021-01-03 18:36:17 +0300

    Error2 Action

--- NetCoreGithubRepo/Controllers/HomeController.cs
+++ NetCoreGithubRepo/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace NetCoreGithubRepo.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error2()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
